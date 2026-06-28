646baa4906997fe37bbb6dbc6a2813e71f80c503
herzie48 <aiswariaben@gmail.com>
2026-06-28 21:28:20 +0530

    Track number of guesses and display message
    Added guess count tracking and congratulatory message.

--- Numguess.py
+++ Numguess.py
@@ -3,10 +3,11 @@ n = random.randint(1,100)
 guess = 0
 while guess != n:
     guess =int(input("Enter a number btw 1-100"))
+    guess_count += 1
     if guess > n:
         print("High!")
     elif guess < n:
         print("Low!")
     else:
         print("You guessed it!")
-        
+        print(f"Congratulations! You guessed the number in {guess_count} guesses.")
